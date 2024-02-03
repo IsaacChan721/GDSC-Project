c245fe46ff3ade240f2d557d98089c5fd4fde6ef
ke1b <kelbyto@gmail.com>
2024-02-03 17:19:34 -0500

    fix

--- example.py
+++ example.py
@@ -7,18 +7,22 @@ youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 # Example API call
 response = youtube.search().list(
-    q='calculus for dummies',
-    part='id,snippet',
-    maxResults=4
+    part="snippet",
+    channelType="any",
+    maxResults=10,
+    order="relevance",
+    q="chemistry|stoichiometry",
+    relevanceLanguage="en",
+    safeSearch="moderate",
+    type="video"
 ).execute()
 
 # Extract video IDs and construct video URLs
 video_urls = []
 for item in response['items']:
-    if item['id']['kind'] == 'youtube#video':
-        video_id = item['id']['videoId']
-        video_url = f"https://www.youtube.com/watch?v={video_id}"
-        video_urls.append(video_url)
+    video_id = item['id']['videoId']
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+    video_urls.append(video_url)
 
 # Open each video URL in a web browser
 for url in video_urls:
